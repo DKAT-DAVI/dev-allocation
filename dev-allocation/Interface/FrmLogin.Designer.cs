7b7b042c787ed147d264463f364f21fb31c01554
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-12-11 23:41:29 -0300

    Drawing the FormLogin

--- dev-allocation/Interface/FrmLogin.Designer.cs
+++ dev-allocation/Interface/FrmLogin.Designer.cs
@@ -28,20 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.ptbMaximize = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlForm = new System.Windows.Forms.Panel();
+            this.lblNewDev = new System.Windows.Forms.Label();
+            this.lblAcessForm = new System.Windows.Forms.Label();
+            this.ptbUser = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txbUser = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.pnlControlbox = new System.Windows.Forms.Panel();
+            this.ptbMinimize = new System.Windows.Forms.PictureBox();
+            this.ptbClose = new System.Windows.Forms.PictureBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.pnlLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMaximize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnlForm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbUser)).BeginInit();
+            this.pnlControlbox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMinimize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbClose)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(4)))), ((int)(((byte)(9)))));
+            this.pnlLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlLogin.Controls.Add(this.ptbMaximize);
+            this.pnlLogin.Controls.Add(this.pictureBox1);
+            this.pnlLogin.Controls.Add(this.pnlForm);
+            this.pnlLogin.Controls.Add(this.pnlControlbox);
+            this.pnlLogin.Location = new System.Drawing.Point(-1, 0);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(745, 467);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // ptbMaximize
+            // 
+            this.ptbMaximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ptbMaximize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.ptbMaximize.Image = ((System.Drawing.Image)(resources.GetObject("ptbMaximize.Image")));
+            this.ptbMaximize.Location = new System.Drawing.Point(688, 5);
+            this.ptbMaximize.Name = "ptbMaximize";
+            this.ptbMaximize.Size = new System.Drawing.Size(25, 25);
+            this.ptbMaximize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbMaximize.TabIndex = 4;
+            this.ptbMaximize.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(365, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlForm
+            // 
+            this.pnlForm.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(35)))), ((int)(((byte)(66)))));
+            this.pnlForm.Controls.Add(this.lblNewDev);
+            this.pnlForm.Controls.Add(this.lblAcessForm);
+            this.pnlForm.Controls.Add(this.ptbUser);
+            this.pnlForm.Controls.Add(this.btnLogin);
+            this.pnlForm.Controls.Add(this.txbUser);
+            this.pnlForm.Controls.Add(this.txbPassword);
+            this.pnlForm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.pnlForm.Location = new System.Drawing.Point(422, 88);
+            this.pnlForm.Name = "pnlForm";
+            this.pnlForm.Size = new System.Drawing.Size(256, 325);
+            this.pnlForm.TabIndex = 5;
+            // 
+            // lblNewDev
+            // 
+            this.lblNewDev.AutoSize = true;
+            this.lblNewDev.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblNewDev.Font = new System.Drawing.Font("Arial", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewDev.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
+            this.lblNewDev.Location = new System.Drawing.Point(81, 294);
+            this.lblNewDev.Name = "lblNewDev";
+            this.lblNewDev.Size = new System.Drawing.Size(104, 16);
+            this.lblNewDev.TabIndex = 3;
+            this.lblNewDev.Text = "New Developer";
+            this.lblNewDev.MouseEnter += new System.EventHandler(this.lblNewDev_MouseEnter);
+            this.lblNewDev.MouseLeave += new System.EventHandler(this.lblNewDev_MouseLeave);
+            // 
+            // lblAcessForm
+            // 
+            this.lblAcessForm.AutoSize = true;
+            this.lblAcessForm.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcessForm.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
+            this.lblAcessForm.Location = new System.Drawing.Point(58, 9);
+            this.lblAcessForm.Name = "lblAcessForm";
+            this.lblAcessForm.Size = new System.Drawing.Size(142, 28);
+            this.lblAcessForm.TabIndex = 4;
+            this.lblAcessForm.Text = "Acess Form";
+            // 
+            // ptbUser
+            // 
+            this.ptbUser.Image = ((System.Drawing.Image)(resources.GetObject("ptbUser.Image")));
+            this.ptbUser.Location = new System.Drawing.Point(73, 50);
+            this.ptbUser.Name = "ptbUser";
+            this.ptbUser.Size = new System.Drawing.Size(113, 109);
+            this.ptbUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbUser.TabIndex = 8;
+            this.ptbUser.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(4)))), ((int)(((byte)(9)))));
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogin.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
+            this.btnLogin.Location = new System.Drawing.Point(13, 251);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(232, 30);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.btnLogin.MouseEnter += new System.EventHandler(this.btnLogin_MouseEnter);
+            this.btnLogin.MouseLeave += new System.EventHandler(this.btnLogin_MouseLeave);
+            // 
+            // txbUser
+            // 
+            this.txbUser.Font = new System.Drawing.Font("Consolas", 14F);
+            this.txbUser.Location = new System.Drawing.Point(13, 176);
+            this.txbUser.Name = "txbUser";
+            this.txbUser.Size = new System.Drawing.Size(232, 29);
+            this.txbUser.TabIndex = 0;
+            this.txbUser.Enter += new System.EventHandler(this.txbUser_Enter);
+            this.txbUser.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txbUser_KeyUp);
+            this.txbUser.Leave += new System.EventHandler(this.txbUser_Leave);
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Font = new System.Drawing.Font("Consolas", 14F);
+            this.txbPassword.Location = new System.Drawing.Point(13, 211);
+            this.txbPassword.MaxLength = 12;
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(232, 29);
+            this.txbPassword.TabIndex = 1;
+            this.txbPassword.Enter += new System.EventHandler(this.txbPassword_Enter);
+            this.txbPassword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txbPassword_KeyUp);
+            this.txbPassword.Leave += new System.EventHandler(this.txbPassword_Leave);
+            // 
+            // pnlControlbox
+            // 
+            this.pnlControlbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+            this.pnlControlbox.Controls.Add(this.ptbMinimize);
+            this.pnlControlbox.Controls.Add(this.ptbClose);
+            this.pnlControlbox.Controls.Add(this.lblLogin);
+            this.pnlControlbox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlControlbox.Location = new System.Drawing.Point(0, 0);
+            this.pnlControlbox.Name = "pnlControlbox";
+            this.pnlControlbox.Size = new System.Drawing.Size(745, 35);
+            this.pnlControlbox.TabIndex = 0;
+            this.pnlControlbox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlControlBox_MouseDown);
+            // 
+            // ptbMinimize
+            // 
+            this.ptbMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ptbMinimize.Image = ((System.Drawing.Image)(resources.GetObject("ptbMinimize.Image")));
+            this.ptbMinimize.Location = new System.Drawing.Point(662, 5);
+            this.ptbMinimize.Name = "ptbMinimize";
+            this.ptbMinimize.Size = new System.Drawing.Size(25, 25);
+            this.ptbMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbMinimize.TabIndex = 3;
+            this.ptbMinimize.TabStop = false;
+            this.ptbMinimize.Click += new System.EventHandler(this.ptbMinimize_Click);
+            this.ptbMinimize.MouseEnter += new System.EventHandler(this.ptbMinimize_MouseEnter);
+            this.ptbMinimize.MouseLeave += new System.EventHandler(this.ptbMinimize_MouseLeave);
+            // 
+            // ptbClose
+            // 
+            this.ptbClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ptbClose.Image = ((System.Drawing.Image)(resources.GetObject("ptbClose.Image")));
+            this.ptbClose.Location = new System.Drawing.Point(714, 5);
+            this.ptbClose.Name = "ptbClose";
+            this.ptbClose.Size = new System.Drawing.Size(25, 25);
+            this.ptbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbClose.TabIndex = 1;
+            this.ptbClose.TabStop = false;
+            this.ptbClose.Click += new System.EventHandler(this.ptbClose_Click);
+            this.ptbClose.MouseEnter += new System.EventHandler(this.ptbClose_MouseEnter);
+            this.ptbClose.MouseLeave += new System.EventHandler(this.ptbClose_MouseLeave);
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.lblLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
+            this.lblLogin.Location = new System.Drawing.Point(11, 8);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(53, 19);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login";
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(744, 466);
+            this.Controls.Add(this.pnlLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(668, 434);
             this.Name = "FrmLogin";
-            this.Text = "FrmLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dev Allocation";
             this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.pnlLogin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMaximize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnlForm.ResumeLayout(false);
+            this.pnlForm.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbUser)).EndInit();
+            this.pnlControlbox.ResumeLayout(false);
+            this.pnlControlbox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbMinimize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbClose)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Panel pnlControlbox;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.PictureBox ptbMaximize;
+        private System.Windows.Forms.PictureBox ptbMinimize;
+        private System.Windows.Forms.PictureBox ptbClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlForm;
+        private System.Windows.Forms.Label lblNewDev;
+        private System.Windows.Forms.Label lblAcessForm;
+        private System.Windows.Forms.PictureBox ptbUser;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txbUser;
+        private System.Windows.Forms.TextBox txbPassword;
     }
 }
--- dev-allocation/Interface/FrmLogin.cs
+++ dev-allocation/Interface/FrmLogin.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,6 +25,37 @@ namespace dev_allocation
         }
         #endregion
 
+
+        #region Move Window
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void pnlControlBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void pnlLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+
+        //-- FrmMain
         public FrmLogin()
         {
             InitializeComponent();
@@ -31,7 +63,152 @@ namespace dev_allocation
 
         private void FrmLogin_Load(object sender, EventArgs e)
         {
+            // Starting the database connection
             Repository dbContext = new Repository();
+
+            this.ActiveControl = lblAcessForm;
+
+            txbUser.Text = "Email";
+            txbUser.ForeColor = Color.Gray;
+
+            txbPassword.Text = "Password";
+            txbPassword.ForeColor = Color.Gray;
+
         }
+        //--//
+
+
+        //-- ControlBox
+        //-- ptbClose
+        private void ptbClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //-- MouseHover
+        private void ptbClose_MouseEnter(object sender, EventArgs e)
+        {
+            ptbClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(35)))), ((int)(((byte)(66)))));
+        }
+        private void ptbClose_MouseLeave(object sender, EventArgs e)
+        {
+            ptbClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+        }
+
+        //-- ptbMinimize
+        private void ptbMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        //-- MouseHover
+        private void ptbMinimize_MouseEnter(object sender, EventArgs e)
+        {
+            ptbMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(35)))), ((int)(((byte)(66)))));
+        }
+        private void ptbMinimize_MouseLeave(object sender, EventArgs e)
+        {
+            ptbMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(16)))), ((int)(((byte)(52)))));
+        }
+        //--//
+
+
+        //-- txbUser
+        //-- Placeholder
+        private void txbUser_Enter(object sender, EventArgs e)
+        {
+            if (txbUser.Text == "Email")
+            {
+                txbUser.Text = "";
+                txbUser.ForeColor = Color.Black;
+            }
+        }
+        private void txbUser_Leave(object sender, EventArgs e)
+        {
+            if (txbUser.Text == "")
+            {
+                txbUser.Text = "Email";
+                txbUser.ForeColor = Color.Gray;
+            }
+        }
+        //-- Switch field
+        private void txbUser_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txbPassword.Focus();
+            }
+        }
+        //--//
+
+
+        //-- txbPassword
+        // Placeholder
+        private void txbPassword_Enter(object sender, EventArgs e)
+        {
+            if (txbPassword.Text == "Password")
+            {
+                txbPassword.Text = "";
+                txbPassword.ForeColor = Color.Black;
+            }
+
+            // HidePassword
+            txbPassword.UseSystemPasswordChar = true;
+        }
+        private void txbPassword_Leave(object sender, EventArgs e)
+        {
+            if (txbPassword.Text == "")
+            {
+                txbPassword.Text = "Password";
+                txbPassword.ForeColor = Color.Gray;
+
+                /// HidePassword
+                txbPassword.UseSystemPasswordChar = false;
+            }
+        }
+        //-- Switch field 
+        private void txbPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnLogin.PerformClick();
+            }
+        }
+        //--//
+
+
+        //-- btnLogin
+        // MouseHover
+        private void btnLogin_MouseEnter(object sender, EventArgs e)
+        {
+            btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(5)))), ((int)(((byte)(20)))));
+            btnLogin.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Bold);
+        }
+        private void btnLogin_MouseLeave(object sender, EventArgs e)
+        {
+            btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(4)))), ((int)(((byte)(9)))));
+            btnLogin.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold);
+        }
+
+        // Click
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Funfou");
+        }
+        //--//
+
+
+        //-- lblNewDev
+        // MouseHover
+        private void lblNewDev_MouseEnter(object sender, EventArgs e)
+        {
+            lblNewDev.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(200)))));
+        }
+        private void lblNewDev_MouseLeave(object sender, EventArgs e)
+        {
+            lblNewDev.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
+        }
+
+        //--//
+
+
     }
 }
